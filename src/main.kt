f7fdbc45ca3708b417ac0e6d7bb16182a7e2f479
EndermanAPM <xavier1997@gmail.com>
2017-04-21 03:17:05 +0200

    Prints time left for the next sale

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,24 @@
+
 import org.openqa.selenium.By
 import org.openqa.selenium.Keys
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import java.io.File
+import java.time.Duration
+import java.time.Instant
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 
 /**
  * Created by Ender on 13/04/2017.
  */
 fun main(args: Array<String>) {
+    val GOCKODRIVER_PATH = """F:\Descargas\selenium-java-3.3.1\geckodriver.exe"""
+
     // GeckoDriver route is necessary for FirefoxDriver to work if it's not set in the path.
-    System.setProperty("webdriver.gecko.driver", """F:\Descargas\selenium-java-3.3.1\geckodriver.exe""")
+    System.setProperty("webdriver.gecko.driver", GOCKODRIVER_PATH)
 
     // Creating Firefox driver object.
     val driver = FirefoxDriver()
@@ -39,6 +46,19 @@ fun main(args: Array<String>) {
     var catt = coin.getAttribute("class")
     if (catt == "coin dead") {
         println("Coin dead, not clickable")
+        var next_sale_datetime = driver.findElementById("deal-timer__time")
+                .getAttribute("datetime")
+        // Gets instant from web element
+        var dateTimeParked = ZonedDateTime.parse(next_sale_datetime, DateTimeFormatter.ISO_OFFSET_DATE_TIME).toInstant()
+
+        val remaining = Duration.between(Instant.now(), dateTimeParked)
+        val output = remaining.toString()
+                .replace("PT", "")
+                .replace("S", " seconds ")
+                .replace("H", " hours ")
+                .replace("M", " minutes ")
+        println("Time untill the next sale: $output")
+
     }
 
 
